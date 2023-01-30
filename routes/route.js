928bcfdbc214a702529100bf7d348746f424c2b1
valentinacamacho <valentinacamacho0206@gmail.com>
2023-01-30 16:22:57 -0500

    Revision of Node.js dependencies and creation of variable that will contain the routhing methods.

--- routes/route.js
+++ routes/route.js
@@ -1,1 +1,11 @@
 //routes method
+
+//import instance of express
+import { express } from "express";
+
+//import properties called from the controller
+import { addMovie, getMovies, getMovie, editMovie, deleteMovie} from '../controller/user-controller.js';
+
+//const variable will contain the methods of the routes
+const router = express.Router();
+
